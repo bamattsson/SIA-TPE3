06cec0bfec164cab615efbc171bae53d148d1bfd
Björn Mattsson <bamattsson@gmail.com>
2015-05-26 20:05:02 -0300

    Adding support for two hidden layers in vectorToCell

--- simpleMutation.m
+++ simpleMutation.m
@@ -1,4 +1,7 @@
 function individual = simpleMutation(individual, possibility, change)
+% individual - the individual in vector format
+% possibility - the possibility that each element is mutated
+% change - how big the change will be if a mutation happens, should be <1
 
 for i = 1:size(individual,2)
   if (rand() < possibility)
--- vectorToCell.m
+++ vectorToCell.m
@@ -4,6 +4,7 @@ function [individual] = vectorToCell(vec, dimension)
 
 individual{1} = zeros(dimension(1), dimension(2));
 individual{2} = zeros(dimension(3), dimension(4));
+individual{3} = zeros(dimension(5), dimension(6));
 
 for i=1:dimension(2);
   individual{1}(:,i) = transpose(vec(1 + (i - 1)*dimension(1):i*dimension(1)));
@@ -11,4 +12,8 @@ end
 
 for i=1:dimension(4);
   individual{2}(:,i) = transpose(vec(1 + dimension(1)*dimension(2) + (i - 1)*dimension(3):dimension(1)*dimension(2) + i*dimension(3)));
+end
+
+for i=1:dimension(6);
+  individual{3}(:,i) = transpose(vec(1 + dimension(1)*dimension(2) + dimension(3)*dimension(4) + (i - 1)*dimension(5):dimension(1)*dimension(2) + dimension(3)*dimension(4) + i*dimension(5)));
 end
