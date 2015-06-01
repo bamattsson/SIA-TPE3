a43f3a348ee0d749f0e246e6a4455c9ebae18462
valeserber <valserber@gmail.com>
2015-06-01 20:10:02 -0300

    crossover anular fixed

--- crossoverAnular.m
+++ crossoverAnular.m
@@ -1,17 +1,19 @@
-function sons = crossoverAnular(parents)
+function sons = crossoverAnular2(parents)
 	totalPositions = size(parents{1},2);
 	r = ceil(rand()*totalPositions);
 	l = ceil(rand()*totalPositions/2);
-	len = r+l;
-	for i=1:(len)
-		if (i < r)
-			sons{1}(i) = parents{1}(i);
-			sons{2}(i) = parents{2}(i);
-		elseif (i >= r && i <= totalPositions)
-			sons{1}(i) = parents{2}(i);
-			sons{2}(i) = parents{1}(i);
-		elseif (i >= r && i > totalPositions)
-			sons{1}(mod(i,totalPositions)) = parents{2}(mod(i,totalPositions));
-			sons{2}(mod(i,totalPositions)) = parents{1}(mod(i,totalPositions));
+	if (r+l == totalPositions)
+		sons{1} = [parents{1}(1:r-1), parents{2}(r:end)];
+		sons{2} = [parents{2}(1:r-1), parents{1}(r:end)];
+	elseif (r+l < totalPositions)
+		sons{1} = [parents{1}(1:r-1), parents{2}(r:r+l), parents{1}(r+l+1:end)];
+		sons{2} = [parents{2}(1:r-1), parents{1}(r:r+l), parents{2}(r+l+1:end)];
+	else
+		aux1 = [parents{1}(1:r-1), parents{2}(r:end)];
+		aux1(1:(l-(totalPositions-r))) = parents{2}(1:(l-(totalPositions-r)));
+		sons{1} = aux1;
+		aux2 = [parents{2}(1:r-1), parents{1}(r:end)];
+		aux2(1:(l-(totalPositions-r))) = parents{1}(1:(l-(totalPositions-r)));
+		sons{2} = aux2;
 	end
 end
